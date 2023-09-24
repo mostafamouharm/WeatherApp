4b716fcb606a2d7c6aabc4c7d57fd5400cfabd2e
mostafamouharm <moustafa.mo7arm@gmail.com>
2023-09-24 19:55:10 +0300

    WeatherApp

--- lib/views/search_view.dart
+++ lib/views/search_view.dart
@@ -1,3 +1,5 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
 
 class SearchView extends StatelessWidget {
@@ -7,7 +9,29 @@ class SearchView extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Search City '),
+        title: const Text('Search City '),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20.0),
+        child: Center(
+          child: TextField(
+            onSubmitted: (value) {
+              log(value);
+            },
+            decoration: InputDecoration(
+              labelText: ' Search ',
+              contentPadding:
+                  const EdgeInsets.symmetric(vertical: 25, horizontal: 16),
+              suffixIcon: const Icon(Icons.search, color: Colors.blue),
+              hintText: 'Enter City Name',
+              hintStyle: const TextStyle(color: Colors.blue),
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(16),
+                borderSide: const BorderSide(color: Colors.orange),
+              ),
+            ),
+          ),
+        ),
       ),
     );
   }
